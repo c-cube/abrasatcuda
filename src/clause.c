16b210100d318a0bdfaf00e4950812840a877331
simon.cruanes <simon.cruanes@34a82d08-26cd-11df-8a64-89189411362a>
2010-03-12 23:20:43 +0000

    removed formula_build, which is finally not useful
    git-svn-id: http://abrasatcuda.googlecode.com/svn/trunk@42 34a82d08-26cd-11df-8a64-89189411362a

--- src/clause.c
+++ src/clause.c
@@ -17,42 +17,6 @@ short make_atom( int n )
 }
 
 
-atom_t formula_build( 
-    atom_t **formula, 
-    atom_t **clauses_index, 
-    atom_t *clauses, 
-    int *clauses_length, 
-    int n )
-{
-    atom_t offset = 0;
-
-    assert( formula != NULL);
-    assert( *formula == NULL);
-    assert( clauses_index != NULL); 
-    assert( *clauses_index == NULL);
-
-    int formula_size = 42 * n  ;
-    *formula = malloc( formula_size * sizeof(atom_t) );
-    *clauses_index = malloc(n * sizeof(atom_t));
-
-    for (int i=0; i<n; ++i){
-        while ( offset + clauses_length[i] >= formula_size ){
-            // allocate more space if needed
-            formula_size = (int) (formula_size * 1.5);
-            *formula = realloc( *formula, formula_size );
-        }
-        
-        // add clause to formula
-        memcpy( *formula + offset, &clauses[i], clauses_length[i] );
-
-        atom_t old_offset = offset; // upgrade offset
-        offset += clauses_length[i]+1;
-
-        (*clauses_index)[i] = old_offset;
-    }
-
-    return offset;
-}
 
 
 
