c9d86170d54dbeba3c0207a032a827ce70ece056
simon.cruanes <simon.cruanes@34a82d08-26cd-11df-8a64-89189411362a>
2010-03-13 16:28:06 +0000

    main() suitable for complete solving cycle
    git-svn-id: http://abrasatcuda.googlecode.com/svn/trunk@49 34a82d08-26cd-11df-8a64-89189411362a

--- src/abrasatcuda.c
+++ src/abrasatcuda.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 #include "parser.h"
+#include "solve.h"
+#include "abrasatcuda.h"
 
 
 
@@ -9,15 +11,63 @@
 
 int main( int argc, char ** argv )
 {
+    
+    if (argc < 2){
+        printf("usage : abrasatcuda file");
+        return 1;
+    }
+
+    char* file_path = argv[1];
+
+    atom_t *formula = NULL;
+    atom_t *clauses_index = NULL;
+    int num_var, num_clause;
+
+    printf("parses file %s", file_path );
+    parse( file_path, &formula, &clauses_index, &num_var, &num_clause );
+
+    printf("file parsed, formula of %d clauses and %d var built\n",
+        num_clause, num_var );
+
+    formula_print( formula, clauses_index, num_clause );
+
+    int answer = solve( formula, clauses_index, num_clause );
+
+    printf("\e[31;4mAnswer : %s\e[m \n", answer ? "Vrai" : "Faux" );
 
 
-
-    printf("it works !\n");
+    // de-allocate memory
+    free(formula);
+    free(clauses_index);
 
     return 0;
 }
 
+
+
+
+/*
+ * this function's purpose is to manage to solve the problem.
+ * It relies on solve_thread (one or many instances) to do so.
+ */
+
+// forward declaration
+int solve_thread( atom_t* formula, atom_t* clauses_index, char* vars, int n );
+
 int solve( atom_t *formula, atom_t* clauses_index, int n )
 {
-    return 0; // TODO 
+    // allocates space for n vars
+    char vars[n];
+
+    return solve_thread( formula, clauses_index, vars, n );
+}
+
+
+
+
+int solve_thread( atom_t* formula, atom_t* clauses_index, char* vars, int n )
+{
+    
+
+    return 0;
 }
