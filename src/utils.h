22a66aae728d24515748c7756bfe981ee4b3e760
simon.cruanes <simon.cruanes@34a82d08-26cd-11df-8a64-89189411362a>
2010-03-16 21:11:50 +0000

    added comments
    git-svn-id: http://abrasatcuda.googlecode.com/svn/trunk@79 34a82d08-26cd-11df-8a64-89189411362a

--- src/utils.h
+++ src/utils.h
@@ -2,9 +2,20 @@
  * thanks to the linux kernel :)
  */
 
+
+/*
+ * finds the offset of [MEMBER] in [TYPE] (which is a struct), by 
+ * casting 0 in (TYPE*) and taking the address of the member.
+ */
+
 #define offsetof(TYPE, MEMBER) ((size_t) &((TYPE *)0)->MEMBER)
 
 
+/*
+ * finds a pointer to the struct of type [type], which member [member] has address 
+ * [ptr]. Its basically allows to find a structure from one of its member, 
+ * assuming you know its type.
+ */
 #define container_of(ptr, type, member) ({                                  \
                 const typeof( ((type *)0)->member ) *__mptr = (ptr);        \
                 (type *)( (char *)__mptr - offsetof(type,member) );})
