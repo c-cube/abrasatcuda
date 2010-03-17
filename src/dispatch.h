4d23ddfffa9af451f4416b1d52682bde16502b16
simon.cruanes <simon.cruanes@34a82d08-26cd-11df-8a64-89189411362a>
2010-03-17 20:56:16 +0000

    interface to change the evaluation parallel dispatching
    git-svn-id: http://abrasatcuda.googlecode.com/svn/trunk@100 34a82d08-26cd-11df-8a64-89189411362a

---
+++ src/dispatch.h
@@ -0,0 +1,41 @@
+/*
+ * This header defines an interface that the main() can use to solve 
+ * a problem. It provides only a function, solve(), which purpose
+ * is to dispatch solving work on one or many threads (or even on
+ * network...) and then gather the results to give them back to 
+ * the caller.
+ * It uses a solve_thread (module implementing solve.h) as a basic 
+ * computing unit.
+ */
+
+
+
+#ifndef DISPATCH_H
+#define DISPATCH_H
+
+#include "consts.h"
+#include "clauses.h"
+#include "vars.h"
+
+
+
+/*
+ * this function's purpose is to manage to solve the problem.
+ * It relies on solve_thread (one or many instances) to do so.
+ */
+
+int solve( atom_t *formula, atom_t* clauses_index, int clause_n, int var_n );
+
+
+    // TODO : create CUDA threads, each with its own [vars] array,
+    // and dispatch it in CUDA.
+    // TODO : find the k most "interesting" vars, and create 2^k threads
+    // with different var affectations.
+    // TODO : think of a way to share information between threads (for example,
+    // if a thread finds out that a var cannot be true (exhausted possibilities ?)
+    // it may tell the other threads to set this var to 0)
+
+
+
+#endif
+
