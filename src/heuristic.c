80fbd7e4d48fae7d463abfd63e84f520ba8cbe66
simon.cruanes <simon.cruanes@34a82d08-26cd-11df-8a64-89189411362a>
2010-03-18 21:58:43 +0000

    deplaced an inline function
    git-svn-id: http://abrasatcuda.googlecode.com/svn/trunk@112 34a82d08-26cd-11df-8a64-89189411362a

--- src/heuristic.c
+++ src/heuristic.c
@@ -3,6 +3,53 @@
 // holds the address of interest table
 static double **interest_ptr = NULL;
 
+/*
+ * This function tries to evaluate how important the var is
+ * in the formula.
+ */
+static inline double
+compute_value( atom_t *formula, atom_t *clauses_index, value_t var, int clause_n )
+{
+    // number of times the var appears positively or negatively in the formula
+    int positive_occur_num = 0;
+    int negative_occur_num = 0;
+
+    for ( int i = 0; i < clause_n; ++i ){
+        
+        atom_t *clause = formula + (clauses_index[i]);
+        atom_t *clause_end = formula + (clauses_index[i+1]);
+
+        // for each atom in the clause, if it is an instance of [var]
+        for ( atom_t *cur_atom = clause; cur_atom < clause_end; ++cur_atom ){
+            if ( VARIABLE_NAME(*cur_atom) == var ){
+
+                if ( IS_NEGATED(*cur_atom) )
+                    negative_occur_num++;
+                else
+                    positive_occur_num++;
+            }
+        }
+
+    }
+
+    // total number of occurrences
+    int total_occur_num = positive_occur_num + negative_occur_num;
+    
+    /*
+     * The heuristic is based on two things :
+     *      the number of times a var appears, which increases its importance;
+     *      the "equilibrium" of those occurrences, ie a var which appears 
+     *          as many times positively and negatively is more likely to
+     *          be of interest in both positive and negative value (remember
+     *          that we try to know which vars are good to be switched on 
+     *          from the beginning)
+     * This is motivated by the assumption that the more a var appears, the earlier
+     * it reveals whether this choice was good or not.
+     */
+
+    return exp( (double) (total_occur_num)/abs( negative_occur_num - positive_occur_num) );
+}
+
 
 // this compares two vars by comparing their "interest value"
 static int 
