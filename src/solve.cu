1679703709bded248d406802c777fc8776aa5dc8
vincent.barrielle <vincent.barrielle@34a82d08-26cd-11df-8a64-89189411362a>
2010-03-20 22:04:14 +0000

    added forgotten wait till all kernels are done
    git-svn-id: http://abrasatcuda.googlecode.com/svn/trunk@154 34a82d08-26cd-11df-8a64-89189411362a

--- src/solve.cu
+++ src/solve.cu
@@ -16,6 +16,14 @@
 */
 
 /*
+* global variables for device memory, as it seems to be required for compilation
+*/
+__device__ atom_t * formula_d;
+__device__ atom_t * clauses_index_d;
+__device__ value_t * vars_affectations_d;
+__device__ truth_t * answers_d;
+
+/*
 * chooses immutable vars and sets them differently for each thread
 */
 __host__ void
@@ -44,29 +52,22 @@ __host__ void
 prepare_gpu_memory( atom_t * formula,  atom_t * formula_d, atom_t * clauses_index,  atom_t * clauses_index_d, value_t * vars_affectations,  value_t * vars_affectations_d, int clause_n, int var_n, truth_t * answers,  truth_t * answers_d, int thread_n)
 {
     // first, we allocate the meomry on the device
-    size_t fomrmula_size = (clauses_index[clause_n] - clauses_index[0]+ 1) * sizeof(atom_t);
+    size_t formula_size = (clauses_index[clause_n] - clauses_index[0]+ 1) * sizeof(atom_t);
     size_t clauses_index_size = clause_n * sizeof(atom_t);
     size_t vars_size = thread_n * (var_n + 1) * sizeof( value_t);
     size_t ans_size = thread_n * sizeof( truth_t);
-    cudaMalloc( (void **) &formula_d, fomrmula_size);
+    cudaMalloc( (void **) &formula_d, formula_size);
     cudaMalloc( (void **) &clauses_index_d, clauses_index_size);
     cudaMalloc( (void **) &vars_affectations_d, vars_size);
     cudaMalloc( (void **) &answers_d, ans_size);
     // now we transfer data to the device
-    cudaMemcpy( formula_d, formula, fomrmula_size, cudaMemcpyHostToDevice);
+    cudaMemcpy( formula_d, formula, formula_size, cudaMemcpyHostToDevice);
     cudaMemcpy( clauses_index_d, clauses_index, clauses_index_size, cudaMemcpyHostToDevice);
     cudaMemcpy( vars_affectations_d, vars_affectations, vars_size, cudaMemcpyHostToDevice);
     cudaMemcpy( answers_d, answers, ans_size, cudaMemcpyHostToDevice);
 
 }
 
-/*
-* global variables for device memory, as it seems to be required for compilation
-*/
-__device__ atom_t * formula_d;
-__device__ atom_t * clauses_index_d;
-__device__ value_t * vars_affectations_d;
-__device__ truth_t * answers_d;
 
 /*
 * this function is a thread solving one of the 2^k variable tries.
@@ -77,7 +78,6 @@ cuda_solve ( atom_t * formula, atom_t * clause_index, value_t * vars_affectation
 {
     int block_id = blockIdx.x * blockDim.x;
     int id_in_block = threadIdx.x;
-    // TODO : retrieve block idee. CRITICAL for retrieving affectations correctly
     extern __shared__ value_t vars_in_global[];
     // TODO : verify this affectation is correct
     for ( int i = 1; i <= var_n; ++i) 
@@ -120,6 +120,8 @@ solve ( atom_t *formula, atom_t* clauses_index, int clause_n, int var_n )
 
   // now we call the cuda kernel to solve each instance
   cuda_solve<<<8,THREAD_NUM/8, shared_mem_size>>> ( formula_d, clauses_index_d, vars_affectations_d, clause_n, var_n, answers_d, THREAD_NUM);
+
+  cudaThreadSynchronize();
 
   cudaMemcpy( answers, answers_d, THREAD_NUM * sizeof(truth_t), cudaMemcpyDeviceToHost);
 
